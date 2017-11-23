f2a43f5b6bbd7a5dd078cb32fc4c17abb270b4e5
Su Nguyen T <su.nguyen@asiantech.vn>
2017-11-23 10:19:35 +0700

    Update.

--- RxSwiftTutorials/3-DeepDiving/3.2.2/CombinationCocoa.swift
+++ RxSwiftTutorials/3-DeepDiving/3.2.2/CombinationCocoa.swift
@@ -11,25 +11,38 @@ import RxCocoa
 import RxSwift
 
 final class CombinationCocoa: UIViewController {
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    @IBOutlet private weak var tapMeButton: UIButton!
+    @IBOutlet private weak var finalResultLabel: UILabel!
 
     let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        combineLatest()
+        setup()
     }
 
-    private func combineLatest() {
-        let t1 = textField1.rx.text.orEmpty
-        let t2 = textField2.rx.text.orEmpty
+    private func setup() {
+        let text1 = textField1.rx.text.orEmpty
+        let text2 = textField2.rx.text.orEmpty
 
-        let observable = Observable<String>.combineLatest(t1, t2) { $0 + " " + $1 }
-        observable
-            .subscribe(onNext: { text in
-                print(text)
-            })
+        // combineLatest
+        let observable = Observable<String>.combineLatest(text1, text2) { $0 + " " +  $1 }
+        observable.bind { [weak self] (value) in
+            guard let this = self else { return }
+            this.resultLabel.text = value
+            }
+            .disposed(by: disposeBag)
+
+        // withLatestFrom
+        let tap = tapMeButton.rx.tap.asObservable()
+        tap.withLatestFrom(observable)
+            .bind { [weak self] (value) in
+                guard let this = self else { return }
+                this.finalResultLabel.text = value
+            }
             .disposed(by: disposeBag)
     }
 }
